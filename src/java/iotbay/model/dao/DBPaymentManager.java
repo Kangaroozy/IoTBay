1b42e75c2d1a33b280a5bddafe4d77c9fd0b0adb
Chunsong123 <79673926+Chunsong123@users.noreply.github.com>
2021-05-21 07:46:01 +0800

    Update DBPaymentManager.java

--- src/java/iotbay/model/dao/DBPaymentManager.java
+++ src/java/iotbay/model/dao/DBPaymentManager.java
@@ -15,24 +15,31 @@ public class DBPaymentManager {
     public DBPaymentManager(Connection conn) throws SQLException {
         st = conn.createStatement();
     }
-    public Payment findPayment(int PayID) throws SQLException {
-        String fetch = "SELECT * FROM IOTUSER.PAYMENT WHERE PAYMENTID=" + PayID + "";
+    public Payment findPayment(int PayID, String Date) throws SQLException {
+        String fetch = "SELECT * FROM IOTUSER.PAYMENT WHERE PAYMENTID=" + PayID + " AND DATE='" + Date + "'";
         ResultSet rs = st.executeQuery(fetch);
         while(rs.next()){
             int paymentID = rs.getInt(1);
-            if (paymentID == paymentID) {
-                String date = rs.getString(2);
+            String date = rs.getString(2);
+            if (paymentID == paymentID && Date.equals(date)) {
+               
                 String paymentMethod = rs.getString(3);
                 int orderID = rs.getInt(4);
                 double paymentAmount = rs.getDouble(5);
                 int cvv = rs.getInt(6);
                 String creditnum = rs.getString(7);
-                return new Payment(paymentID, date, paymentMethod, orderID, paymentAmount, cvv, creditnum );
+                return new Payment(paymentID, date, paymentMethod,paymentAmount,  orderID, cvv, creditnum );
             }
         }return null;
     }
-    public void addPayment(String date,int orderID, int paymentID, double paymentAmount, String paymentMethod, int cvv, String creditnum) throws SQLException {
-        st.executeUpdate("INSERT INTO IOTUSER.PAYMENT DATE, ORDERID, PAYMENTID, PAYMENTAMOUNT, PAYMENTMETHOD, CVV,CREDITNUMBER " + "VALUES('" + date +"','" + orderID +"', '" + paymentID + "','" + paymentAmount + "','" + paymentMethod + "', '" + cvv + "','" + creditnum + "')");
+     
+  
+   
+    public void addPayment(String date,int orderID, double paymentAmount, String paymentMethod, int cvv, String creditnum) throws SQLException {
+        String columns = "INSERT INTO IOTUSER.PAYMENT(PAYMENTMETHOD,PAYMENTAMOUNT,ORDERID,CVV,CREDITNUM,\"DATE\")";
+        String values = "VALUES('"+paymentMethod+"',"+paymentAmount+","+orderID+","+cvv+",'"+creditnum+"','"+ date+"')";
+        st.executeUpdate(columns+values);
+        //st.executeUpdate("INSERT INTO IOTUSER.PAYMENT DATE, ORDERID, PAYMENTID, PAYMENTAMOUNT, PAYMENTMETHOD, CVV,CREDITNUMBER " + "VALUES('" + date +"','" + orderID +"', '" + paymentID + "','" + paymentAmount + "','" + paymentMethod + "', '" + cvv + "','" + creditnum + "')");
     }
 
      public Payment readPayment(int PaymentID, String Date) throws SQLException {
@@ -48,10 +55,10 @@ public class DBPaymentManager {
                 double paymentAmount = rs.getDouble(5);
                 int cvv = rs.getInt(6);
                 String creditnum = rs.getString(7);
-            payments.add(new Payment(paymentID, date, paymentMethod, orderID, paymentAmount, cvv, creditnum));
+            payments.add(new Payment(paymentID, date, paymentMethod, paymentAmount,orderID,  cvv, creditnum));
         }
         return null;
-    }    
+    }  
     
 //update a user details in the database   
     public void updatePayment(int paymentID, String paymentMethod, int orderID, double paymentAmount, int cvv, String creditnum) throws SQLException {
@@ -60,7 +67,7 @@ public class DBPaymentManager {
 
 //delete a user from the database   
     public void deletePayment(int paymentID) throws SQLException {
-        st.executeUpdate("DELETE FROM IOTUSER.PAYMENT WHERE PAYMENTID=" + paymentID + "");
+        st.executeUpdate("DELETE FROM IOTUSER.PAYMENT WHERE PAYMENTID=" + paymentID + " ");
     }
     public ArrayList<Payment> fetchPayment() throws SQLException {
         ResultSet rs = st.executeQuery("SELECT * FROM IOTUSER.PAYMENT");
@@ -68,13 +75,13 @@ public class DBPaymentManager {
 
         while (rs.next()) {
             int paymentID = rs.getInt(1);
+            String paymentMethod = rs.getString(3);
                 String date = rs.getString(2);
-                String paymentMethod = rs.getString(3);
-                int orderID = rs.getInt(4);
-                double paymentAmount = rs.getDouble(5);
+                double paymentAmount = rs.getDouble(4);
+                int orderID = rs.getInt(5);                
                 int cvv = rs.getInt(6);
                 String creditnum = rs.getString(7);
-            payments.add(new Payment(paymentID, date, paymentMethod, orderID, paymentAmount, cvv, creditnum));
+            payments.add(new Payment(paymentID, date, paymentMethod, paymentAmount,orderID,  cvv, creditnum));
         }
         return payments;
     }
@@ -94,23 +101,19 @@ public class DBPaymentManager {
         while (rs.next()) {
             int paymentID = rs.getInt(1);
             String date = rs.getString(2);
-                String paymentMethod = rs.getString(3);
-                int orderID = rs.getInt(4);
-                double paymentAmount = rs.getDouble(5);
-                int cvv = rs.getInt(6);
-                String creditnum = rs.getString(7);
-            payments.add(new Payment(paymentID, date, paymentMethod, orderID, paymentAmount, cvv, creditnum ));
+            String paymentMethod = rs.getString(3);
+            int orderID = rs.getInt(4);
+            double paymentAmount = rs.getDouble(5);
+            int cvv = rs.getInt(6);
+            String creditnum = rs.getString(7);
+            payments.add(new Payment(paymentID, date, paymentMethod, paymentAmount,orderID,  cvv, creditnum ));
         }
         return payments;
     }
 
-    public void addPayment(String date, int orderID, int paymentID, String paymentAmount, String paymentMethod, int cvv, int creditnum) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    public void addPayment(int orderID, int paymentID, double paymentAmount, String paymentMethod, int cvv, String creditnum) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
+ 
 
 
 }
